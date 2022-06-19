8579b256d901a01d1ba7e86cfc72aeba303a6458
Wiktor Pabich <pabichwiktor@gmail.com>
2022-06-19 09:49:47 +0200

    feat: migrate to nodejs

---
+++ spotlight_upload.js
@@ -0,0 +1,36 @@
+const { createClient } = require('@supabase/supabase-js');
+const fs = require('fs');
+
+require('dotenv').config()
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY)
+const path = '/Users/flwn/Downloads/Spotlight Collection'; 
+
+fs.readdir(path, (err, files) => {
+    for(const filename of files) {
+        console.log('File:', filename);
+        fs.readFile(`${path}/${filename}`, async (err, fileData) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+
+            const uploadData = await supabase.storage.from('spotlight-photos').upload(
+                `public/${filename}`, 
+                fileData,
+                { 
+                    contentType: "image/jpeg",
+                    cacheControl: '3600',
+                    upsert: false
+                }
+            )   
+            console.log('upload', uploadData);
+            const { data: { publicURL } } = supabase.storage.from('spotlight-photos').getPublicUrl(`public/${filename}`)
+            console.log('publicURL', publicURL);
+            console.log('inserting...');
+            await supabase.from('spotlight_photos').insert([
+                { "id": filename, "url": publicURL }
+            ])
+            console.log('[DONE]', filename)
+        });
+    };
+});
